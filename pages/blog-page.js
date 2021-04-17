9beb0ae34d7cc4d057b3f390f685f8298902f149
arataoka <anewfantasticpointofview62@gmail.com>
2021-04-17 15:06:53 +0900

    complete

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import Layout from "../components/layout"
+import {getAllPostsData} from "../lib/posts"
+import Post from "../components/post"
 
-const Blog = () => {
+const Blog = ({posts}) => {
     return (
-        <div>
-            Blog
-        </div>
+        <Layout title="BLog">
+            <ul className="m-10">
+                {posts && posts.map(post =><Post key={post.id} post={post}/>)}
+            </ul>
+        </Layout>
     );
 };
 
 export default Blog;
+
+
+export async function getStaticProps(){
+    const posts = await getAllPostsData();
+    return {
+        props:{posts},
+    }
+}
